c069c0bacadb9a5c75d8b3b3d49c04981462fdad
Scott Musson <smusson@salesforce.com>
2012-10-25 13:56:07 -0600

    Updated how the password is used, first it checks the env variable P4PASSWD, if not there it looks for -DP4PASSWD, if not there it looks for password in the properties file.

--- src/main/java/com/sf/checkinactivity/P4Checkins.java
+++ src/main/java/com/sf/checkinactivity/P4Checkins.java
@@ -1,5 +1,6 @@
 package com.sf.checkinactivity;
 
+import com.perforce.p4java.PropertyDefs;
 import com.perforce.p4java.core.IChangelist;
 import com.perforce.p4java.core.IChangelistSummary;
 import com.perforce.p4java.core.file.FileSpecBuilder;
@@ -17,6 +18,7 @@ public class P4Checkins {
     private IServer server;
     public static final String PROPERTIES_FILE_KEY = "repoprops";
     public static final String PROPERTIES_FILE = "/com/sf/checkinactivity/repo.properties";
+    public static final String P4PASSWD = "P4PASSWD";
 
     public P4Checkins() {
     }
@@ -41,6 +43,18 @@ public class P4Checkins {
                 String sName = props.getProperty("servername");
                 server = ServerFactory.getServer(sName, props);
                 server.connect();
+                // First check the P4PASSWD environment variable
+                String password = System.getenv().get(P4PASSWD);
+                // if not there grab it from -D
+                // if not there, try the properties file for the password
+                if (password == null || password.isEmpty()) {
+                    password = System.getProperty(P4PASSWD, props.getProperty(PropertyDefs.PASSWORD_KEY_SHORTFORM));
+                }
+                if (password != null && !password.isEmpty()) {
+                    server.login(password);
+                } else {
+                    throw new IOException("no p4 password provided");
+                }
             } else {
                 throw new FileNotFoundException("Properties file was not found: " + PROPERTIES_FILE);
             }
@@ -60,5 +74,11 @@ public class P4Checkins {
     public IChangelist describeChangelist(IChangelistSummary iChangelistSummary)
             throws AccessException, RequestException, ConnectionException {
         return server.getChangelist(iChangelistSummary.getId());
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        server.logout();
     }
 }
